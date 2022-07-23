a76ef3e246ed626ef0989c71056c5e2fa5c2181b
sprashantofficial <68085442+sprashantofficial@users.noreply.github.com>
2022-07-24 02:30:08 +0530

    New Converter

--- src/main/java/ExcelToMultiplePDFsConverter.java
+++ src/main/java/ExcelToMultiplePDFsConverter.java
@@ -5,9 +5,7 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
@@ -19,31 +17,43 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExcelToPDFConverter {
+public class ExcelToMultiplePDFsConverter {
 
     public static void main(String[] args) throws IOException, DocumentException {
-        FileInputStream fileInputStream = new FileInputStream(new File("C:\\Temp\\Test.xlsx"));
+        FileInputStream fileInputStream = new FileInputStream(new File("C:\\Temp\\file_example_XLSX_10.xlsx"));
         Workbook workbook = new XSSFWorkbook(fileInputStream);
 
         Sheet sheet = workbook.getSheetAt(0);
-        List<String> headerList = getRow(0, sheet);
-
-        Document document = new Document();
-        String fileName = "C:\\Temp\\ConvertedPDF.pdf";
-        PdfWriter.getInstance(document, new FileOutputStream(fileName));
-
-        document.open();
-        PdfPTable table = new PdfPTable(sheet.getRow(0).getPhysicalNumberOfCells());
-        addPDFData(true, headerList, table);
+        List<String> headerList = setHeader(sheet);
 
         for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
-            List<String> rowList = getRow(i, sheet);
+            Document document = new Document();
+            String fileName = "C:\\Temp\\pdf_" + i + ".pdf";
+            PdfWriter.getInstance(document, new FileOutputStream(fileName));
+
+            document.open();
+            PdfPTable table = new PdfPTable(sheet.getRow(0).getPhysicalNumberOfCells());
+            addPDFData(true, headerList, table);
+            List<String> rowList = getRowData(i, sheet);
+            if (rowList.isEmpty()) {
+                document.add(table);
+                document.close();
+                Files.delete(Paths.get(fileName));
+                continue;
+            }
             addPDFData(false, rowList, table);
             document.add(table);
-            table = new PdfPTable(sheet.getRow(0).getPhysicalNumberOfCells());
+            document.close();
         }
 
-        document.close();
+    }
+
+    public static List<String> setHeader(Sheet sheet) {
+        return getRow(0, sheet);
+    }
+
+    public static List<String> getRowData(int index, Sheet sheet) {
+        return getRow(index, sheet);
     }
 
     public static List<String> getRow(int index, Sheet sheet) {
